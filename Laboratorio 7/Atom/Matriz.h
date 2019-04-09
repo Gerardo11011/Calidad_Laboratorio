8156435720e9d81aac5ade5e8fde1549c515f536
Gerardo11011 <gerardo_apg_97@hotmail.com>
2019-04-09 03:56:58 -0500

    cambios esteticos en los comentarios

--- Laboratorio 7/Atom/Matriz.h
+++ Laboratorio 7/Atom/Matriz.h
@@ -19,6 +19,7 @@ class Matriz {
 
     public:
     void calcuRenglones(vector<float>& NumW, vector<float>& NumX, vector<float>& NumY, vector<float>& NumZ, float totales, float matriz[][5]) {
+      ////////RENGLON 1////////
       //B0
       matriz[0][0] = totales;
       float aux = 0;
@@ -36,32 +37,37 @@ class Matriz {
       }
       matriz[0][2] = aux;
       aux = 0;
+
       //B3
       for (int i = 0; i < totales; i++) {
         aux = aux + NumY[i];
       }
       matriz[0][3] = aux;
       aux = 0;
+
       //B4
       for (int i = 0; i < totales; i++) {
         aux = aux + NumZ[i];
       }
       matriz[0][4] = aux;
 
-      aux = 0;
-      ///RENGLON 2
+
+      ////////RENGLON 2////////
       //B0
+      aux = 0;
       for (int i = 0; i < totales; i++) {
         aux = aux + NumW[i];
       }
       matriz[1][0] = aux;
       aux = 0;
+
       //B1
       for (int i = 0; i < totales; i++) {
         aux = aux + pow(NumW[i],2);
       }
       matriz[1][1] = aux;
       aux = 0;
+
       //B2
       for (int i = 0; i < totales; i++) {
         aux = aux + NumX[i] * NumW[i];
@@ -69,6 +75,7 @@ class Matriz {
 
       matriz[1][2] = aux;
       aux = 0;
+
       //B3
       for (int i = 0; i < totales; i++) {
         aux = aux + NumY[i] * NumW[i];
@@ -76,26 +83,29 @@ class Matriz {
 
       matriz[1][3] = aux;
       aux = 0;
+      
       //B4
       for (int i = 0; i < totales; i++) {
         aux = aux + NumZ[i] * NumW[i];
       }
       matriz[1][4] = aux;
 
-      ////RENGLON 3
-      aux = 0;
+      ////////RENGLON 3////////
       //B0
+      aux = 0;
       for (int i = 0; i < totales; i++) {
         aux = aux + NumX[i];
       }
       matriz[2][0] = aux;
       aux = 0;
+
       //B1
       for (int i = 0; i < totales; i++) {
         aux = aux + NumW[i] * NumX[i];
       }
       matriz[2][1] = aux;
       aux = 0;
+
       //B2
       for (int i = 0; i < totales; i++) {
         aux = aux + pow(NumX[i], 2);
@@ -103,6 +113,7 @@ class Matriz {
 
       matriz[2][2] = aux;
       aux = 0;
+
       //B3
       for (int i = 0; i < totales; i++) {
         aux = aux + NumY[i] * NumX[i];
@@ -110,6 +121,7 @@ class Matriz {
 
       matriz[2][3] = aux;
       aux = 0;
+
       //B4
       for (int i = 0; i < totales; i++) {
         aux = aux + NumZ[i] * NumX[i];
@@ -117,20 +129,22 @@ class Matriz {
       matriz[2][4] = aux;
 
 
-      ///RENGLON 4
-      aux = 0;
+      ////////RENGLON 4////////
       //B0
+      aux = 0;
       for (int i = 0; i < totales; i++) {
         aux = aux + NumY[i];
       }
       matriz[3][0] = aux;
       aux = 0;
+
       //B1
       for (int i = 0; i < totales; i++) {
         aux = aux + NumW[i] * NumY[i];
       }
       matriz[3][1] = aux;
       aux = 0;
+
       //B2
       for (int i = 0; i < totales; i++) {
         aux = aux + NumX[i] * NumY[i];
@@ -146,6 +160,7 @@ class Matriz {
 
       matriz[3][3] = aux;
       aux = 0;
+
       //B4
       for (int i = 0; i < totales; i++) {
         aux = aux + NumZ[i] * NumY[i];
